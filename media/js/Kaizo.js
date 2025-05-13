862ac44d4e9b9f172391152f4da308f6be176bc3
Elivelton <fieldsolutionsadm@gmail.com>
2025-05-13 00:59:11 -0400

    Versao claro e escuro 0.09
    foi adicionado novos css e javascripts para implementar as funcionalidades de tema claro e escuro atraves do menu de navegacao

---
+++ media/js/Kaizo.js
@@ -0,0 +1,16 @@
+
+//false = claro
+//true = escuro
+
+var tema = true;
+const link = document.getElementById('themeStyle');
+
+function mudarTema(){
+
+    tema = !tema;
+    if (tema === false) {
+        link.href = "media/css/dark.css"
+    }else{
+        link.href = "media/css/light.css"
+    }
+}
