ae9968d990203d6048fc14b5da3ea93699628e05
Elivelton <fieldsolutionsadm@gmail.com>
2025-05-16 19:30:38 -0400

    theme icon
    trocando o icone para mudanca de thema

--- media/js/Kaizo.js
+++ media/js/Kaizo.js
@@ -4,13 +4,15 @@
 
 var tema = true;
 const link = document.getElementById('themeStyle');
+const img = document.getElementById('btn-tema');
 
 function mudarTema(){
 
     tema = !tema;
     if (tema === false) {
-        link.href = "media/css/dark.css"
+        link.href = "media/css/dark.css";
     }else{
-        link.href = "media/css/light.css"
+        link.href = "media/css/light.css";
+
     }
 }
