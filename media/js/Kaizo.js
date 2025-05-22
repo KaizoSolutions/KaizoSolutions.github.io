75d45fe780a237f4a0327f21ca704dc106305246
Elivelton <fieldsolutionsadm@gmail.com>
2025-05-21 22:21:57 -0400

    Altera;ao das imgs
    alteramos os textos e imgs da parte do grid

--- media/js/Kaizo.js
+++ media/js/Kaizo.js
@@ -16,3 +16,15 @@ function mudarTema(){
 
     }
 }
+
+const images = [
+    'media/imgs/hero1.jpg',
+    'media/imgs/hero2.jpg',
+    'media/imgs/hero3.jpg',
+    'media/imgs/hero4.jpg'
+]
+
+const header = document.querySelector('header');
+const randomIndex = Math.floor(Math.random() * images.length);
+
+header.style.background = `url('${images[randomIndex]}') no-repeat center center/cover`;
